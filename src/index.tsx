0d868f6025c405aa8dc454673d26f47bb1bec3b4
GeonwooShin <rjsdnslawkd@naver.com>
2024-04-08 18:09:18 +0900

    feat: 애플리케이션 스타일 설정 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import GlobalStyles from '@styles/GlobalStyles';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
@@ -8,6 +9,7 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <>
+    <GlobalStyles />
     <App />
   </>,
 );
